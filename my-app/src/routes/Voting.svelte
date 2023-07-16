1168ff1ef661625bba25b5e22af85f7150b086be
Alex@gdplace.com <Alex@gdplace.com>
2023-07-16 02:31:57 -0400

    Added autocomplete

--- my-app/src/routes/Voting.svelte
+++ my-app/src/routes/Voting.svelte
@@ -1,11 +1,13 @@
 <script>
     import { onMount } from 'svelte';
+    import AutoComplete from "simple-svelte-autocomplete"
   
     let players = ["", "", "", ""];
     let comments = ["", "", "", ""];
     let votes = [false, false, false, false];
     let didv;
     let user = "";
+    const allPlayers = ["Afghanistan","Albania","Algeria","Andorra","Angola","Anguilla","Antigua &amp; Barbuda","Argentina","Armenia","Aruba","Australia","Austria","Azerbaijan","Bahamas","Bahrain","Bangladesh","Barbados","Belarus","Belgium","Belize","Benin","Bermuda","Bhutan","Bolivia","Bosnia &amp; Herzegovina","Botswana","Brazil","British Virgin Islands","Brunei","Bulgaria","Burkina Faso","Burundi","Cambodia","Cameroon","Cape Verde","Cayman Islands","Chad","Chile","China","Colombia","Congo","Cook Islands","Costa Rica","Cote D Ivoire","Croatia","Cruise Ship","Cuba","Cyprus","Czech Republic","Denmark","Djibouti","Dominica","Dominican Republic","Ecuador","Egypt","El Salvador","Equatorial Guinea","Estonia","Ethiopia","Falkland Islands","Faroe Islands","Fiji","Finland","France","French Polynesia","French West Indies","Gabon","Gambia","Georgia","Germany","Ghana","Gibraltar","Greece","Greenland","Grenada","Guam","Guatemala","Guernsey","Guinea","Guinea Bissau","Guyana","Haiti","Honduras","Hong Kong","Hungary","Iceland","India","Indonesia","Iran","Iraq","Ireland","Isle of Man","Israel","Italy","Jamaica","Japan","Jersey","Jordan","Kazakhstan","Kenya","Kuwait","Kyrgyz Republic","Laos","Latvia","Lebanon","Lesotho","Liberia","Libya","Liechtenstein","Lithuania","Luxembourg","Macau","Macedonia","Madagascar","Malawi","Malaysia","Maldives","Mali","Malta","Mauritania","Mauritius","Mexico","Moldova","Monaco","Mongolia","Montenegro","Montserrat","Morocco","Mozambique","Namibia","Nepal","Netherlands","Netherlands Antilles","New Caledonia","New Zealand","Nicaragua","Niger","Nigeria","Norway","Oman","Pakistan","Palestine","Panama","Papua New Guinea","Paraguay","Peru","Philippines","Poland","Portugal","Puerto Rico","Qatar","Reunion","Romania","Russia","Rwanda","Saint Pierre &amp; Miquelon","Samoa","San Marino","Satellite","Saudi Arabia","Senegal","Serbia","Seychelles","Sierra Leone","Singapore","Slovakia","Slovenia","South Africa","South Korea","Spain","Sri Lanka","St Kitts &amp; Nevis","St Lucia","St Vincent","St. Lucia","Sudan","Suriname","Swaziland","Sweden","Switzerland","Syria","Taiwan","Tajikistan","Tanzania","Thailand","Timor L'Este","Togo","Tonga","Trinidad &amp; Tobago","Tunisia","Turkey","Turkmenistan","Turks &amp; Caicos","Uganda","Ukraine","United Arab Emirates","United Kingdom","Uruguay","Uzbekistan","Venezuela","Vietnam","Virgin Islands (US)","Yemen","Zambia","Zimbabwe"];
     import { voted, currentDisplay, results, username } from "./stores.js";
 
 	voted.subscribe((voted) => {
@@ -43,7 +45,7 @@
       console.log(results);
       // results.set(players.filter((player, index) => votes[index] !== false));
       votes = votes.map(() => true);
-      voted.set(true);
+      // voted.set(true);
       currentDisplay.set(1);
       saveVotingData();
     }
@@ -82,6 +84,7 @@
         players = ["", "", "", ""];
         comments = ["", "", "", ""];
         votes = [false, false, false, false];
+        voted.set = false;
         localStorage.removeItem('votingData');
 }
 
@@ -99,8 +102,17 @@
     {/if}
   </div>
 
-  <input type="text" disabled={votes[index]} bind:value={players[index]} on:input={(e) => handleChange(e, index)} />
-  <input type="text" disabled={votes[index]} bind:value={comments[index]} on:input={(e) => handleComment(e, index)} />
+  <!-- <input type="text" disabled={votes[index]} bind:value={players[index]} on:input={(e) => handleChange(e, index)} /> -->
+
+  <AutoComplete
+  items="{allPlayers}"
+  placeholder={players[index]}
+  bind:value={players[index]}
+  hideArrow={true}
+  disabled={votes[index]}
+/>
+
+  <input type="text" disabled={votes[index]} bind:value={comments[index]}/>
   <button on:click={() => { handleVote(index); handleComment(index); }}>{votes[index] ? "Undo Vote" : "Save Vote"}</button>
   
   {/each}
@@ -125,6 +137,7 @@
     cursor: not-allowed;
     border: 2px solid red;
 }
+
 
 
 h1, h2 {
@@ -190,4 +203,7 @@ h1, h2 {
     .container:last-child {
       margin-bottom: 0;
     }
+
+
+
   </style>
