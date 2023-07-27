5f3d1dfe95beacee228ef9ce8c1a92a94094287d
Alex@gdplace.com <Alex@gdplace.com>
2023-07-26 22:19:01 -0400

    images and header logic

--- my-app/src/routes/Header.svelte
+++ my-app/src/routes/Header.svelte
@@ -31,6 +31,8 @@ username.subscribe((value) => {
 
 
 <header class="bg-teal-500 h-16 flex justify-center items-center px-4 ">
+  <img class="h-14 absolute left-4" src="https://mlbawards.xyz/logo.png" alt="Logo">
+  
   <nav class="flex items-center space-x-4 ">
     <button class="px-4 py-2 bg-teal-600 text-white rounded-l-lg hover:bg-teal-700" on:click={() => handleClick(0)}>Vote</button>
     <button class="px-4 py-2 bg-teal-600 text-white hover:bg-teal-700" on:click={() => handleClick(2)}>Past Ballots</button>
@@ -38,8 +40,9 @@ username.subscribe((value) => {
     <button class="px-4 py-2 bg-teal-600 text-white rounded-r-lg hover:bg-teal-700" on:click={() => handleClick(3)}>Login</button>
   </nav>
 
-
+  {#if user != ""}
   <button class="px-4 py-2 bg-teal-600 text-white hover:bg-teal-700 rounded-lg absolute right-2" on:click={() => dropDown = !dropDown}>{user}</button>
+  {/if}
   {#if dropDown}
   <ul class="absolute top-16 right-4 bg-teal-600 text-white rounded-lg shadow-md">
     <li class="hover:bg-teal-700 rounded-lg">
--- my-app/src/routes/Login.svelte
+++ my-app/src/routes/Login.svelte
@@ -15,27 +15,40 @@
 <img class="mt-6" src="https://mlbawards.xyz/logo.png" alt="Logo">
 
 <div class="absolute top-1/3"> 
-<form class="max-w-sm mx-auto p-4 bg-white rounded border-black border-2" on:submit|preventDefault={handleSubmit}>
-  <table class="w-full">
-    <thead>
-      <tr>
-        <th class="py-2">Username:</th>
-        <th class="py-2">Password:</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr>
-        <td class="py-2"><input type="text" class="w-full px-4 py-2 border rounded focus:outline-none focus:ring focus:border-blue-500" bind:value={user} /></td>
-        <td class="py-2"><input type="password" class="w-full px-4 py-2 border rounded focus:outline-none focus:ring focus:border-blue-500" bind:value={password} /></td>
-      </tr>
-      <tr>
-        <td class="py-2" colspan="2">
-          <button type="submit" class="w-full bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 focus:outline-none focus:ring focus:border-blue-500">
-            Login
-          </button>
-        </td>
-      </tr>
-    </tbody>
-  </table>
-</form>
+  <form class="max-w-sm mx-auto p-4 bg-white rounded border-black border-2" on:submit|preventDefault={handleSubmit}>
+    <table class="w-full">
+      <thead>
+        <tr>
+          <th class="py-2">Username:</th>
+          <th class="py-2">Password:</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td class="py-2">
+            <input
+              type="text"
+              class="w-full px-4 py-2 border rounded focus:outline-none focus:ring focus:border-blue-500"
+              bind:value={user}
+              maxlength="20" 
+            />
+          </td>
+          <td class="py-2">
+            <input
+              type="password"
+              class="w-full px-4 py-2 border rounded focus:outline-none focus:ring focus:border-blue-500"
+              bind:value={password} maxlength="32"
+            />
+          </td>
+        </tr>
+        <tr>
+          <td class="py-2" colspan="2">
+            <button type="submit" class="w-full bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 focus:outline-none focus:ring focus:border-blue-500">
+              Login
+            </button>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </form>
 </div>
