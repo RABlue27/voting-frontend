d8bdb6bcc6449a6338d1dbb11d1a5e097e5e7eed
Alex@gdplace.com <Alex@gdplace.com>
2023-07-28 01:15:50 -0400

    fixed redundent br url params

--- my-app/src/routes/Voting.svelte
+++ my-app/src/routes/Voting.svelte
@@ -9,7 +9,6 @@
     let user = "";
     const allPlayers = ["Afghanistan","Albania","Algeria","Andorra","Angola","Anguilla","Antigua &amp; Barbuda","Argentina","Armenia","Aruba","Australia","Austria","Azerbaijan","Bahamas","Bahrain","Bangladesh","Barbados","Belarus","Belgium","Belize","Benin","Bermuda","Bhutan","Bolivia","Bosnia &amp; Herzegovina","Botswana","Brazil","British Virgin Islands","Brunei","Bulgaria","Burkina Faso","Burundi","Cambodia","Cameroon","Cape Verde","Cayman Islands","Chad","Chile","China","Colombia","Congo","Cook Islands","Costa Rica","Cote D Ivoire","Croatia","Cruise Ship","Cuba","Cyprus","Czech Republic","Denmark","Djibouti","Dominica","Dominican Republic","Ecuador","Egypt","El Salvador","Equatorial Guinea","Estonia","Ethiopia","Falkland Islands","Faroe Islands","Fiji","Finland","France","French Polynesia","French West Indies","Gabon","Gambia","Georgia","Germany","Ghana","Gibraltar","Greece","Greenland","Grenada","Guam","Guatemala","Guernsey","Guinea","Guinea Bissau","Guyana","Haiti","Honduras","Hong Kong","Hungary","Iceland","India","Indonesia","Iran","Iraq","Ireland","Isle of Man","Israel","Italy","Jamaica","Japan","Jersey","Jordan","Kazakhstan","Kenya","Kuwait","Kyrgyz Republic","Laos","Latvia","Lebanon","Lesotho","Liberia","Libya","Liechtenstein","Lithuania","Luxembourg","Macau","Macedonia","Madagascar","Malawi","Malaysia","Maldives","Mali","Malta","Mauritania","Mauritius","Mexico","Moldova","Monaco","Mongolia","Montenegro","Montserrat","Morocco","Mozambique","Namibia","Nepal","Netherlands","Netherlands Antilles","New Caledonia","New Zealand","Nicaragua","Niger","Nigeria","Norway","Oman","Pakistan","Palestine","Panama","Papua New Guinea","Paraguay","Peru","Philippines","Poland","Portugal","Puerto Rico","Qatar","Reunion","Romania","Russia","Rwanda","Saint Pierre &amp; Miquelon","Samoa","San Marino","Satellite","Saudi Arabia","Senegal","Serbia","Seychelles","Sierra Leone","Singapore","Slovakia","Slovenia","South Africa","South Korea","Spain","Sri Lanka","St Kitts &amp; Nevis","St Lucia","St Vincent","St. Lucia","Sudan","Suriname","Swaziland","Sweden","Switzerland","Syria","Taiwan","Tajikistan","Tanzania","Thailand","Timor L'Este","Togo","Tonga","Trinidad &amp; Tobago","Tunisia","Turkey","Turkmenistan","Turks &amp; Caicos","Uganda","Ukraine","United Arab Emirates","United Kingdom","Uruguay","Uzbekistan","Venezuela","Vietnam","Virgin Islands (US)","Yemen","Zambia","Zimbabwe"];
     import { voted, currentDisplay, username, results } from "./stores.js";
-    import { get } from 'svelte/store';
 
 	voted.subscribe((voted) => {
     voted ? didv = true: didv = false;
@@ -126,7 +125,7 @@ function getNdaysBR(level, type, n) {
   const lastWeekMonth = String(lastWeekDate.getMonth() + 1).padStart(2, '0');
   const lastWeekDay = String(lastWeekDate.getDate()).padStart(2, '0');
 
-  const modifiedURL = `https://www.baseball-reference.com/leagues/daily.fcgi?request=1&type=${type}&user_team=&bust_cache=&dates=lastndays&lastndays=${n}&since=${lastWeekYear}-${lastWeekMonth}-${lastWeekDay}&fromandto=${lastWeekYear}-${lastWeekMonth}-${lastWeekDay}.${currentYear}-${currentMonth}-${currentDay}&level=${level}&franch=ANY`;
+  const modifiedURL = `https://www.baseball-reference.com/leagues/daily.fcgi?request=1&type=${type}&user_team=&bust_cache=&dates=lastndays&lastndays=${n}&level=${level}&franch=ANY`;
   return modifiedURL;
 }
 
