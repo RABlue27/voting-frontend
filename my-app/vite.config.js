93dd11aea0325e0add8269ca65d54bdc69e61bd1
Alex@gdplace.com <Alex@gdplace.com>
2023-08-02 00:38:55 -0400

    dependency

--- my-app/vite.config.js
+++ my-app/vite.config.js
@@ -2,5 +2,14 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+  plugins: [
+    sveltekit(),
+    // Other plugins you might have
+  ],
+  build: {
+    rollupOptions: {
+      external: ['simple-svelte-autocomplete'],
+      // Other rollup options if needed
+    },
+  },
 });
