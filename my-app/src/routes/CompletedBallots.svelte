932650854da26e9332557036ee723b5fcf426f41
Alex@gdplace.com <Alex@gdplace.com>
2023-08-02 04:24:32 -0400

    added monthly toggles for showing off

--- my-app/src/routes/CompletedBallots.svelte
+++ my-app/src/routes/CompletedBallots.svelte
@@ -1,89 +1,174 @@
 <script>
 
-import {onMount} from 'svelte';
-// import data from API endpoint 
-let data = {
-  "Year": 2023,
-  "Weeks": [
-    {
-      "WeekNumber": "1/2",
-      "Players": [
-        {
-          "Name": "Mike Trout",
-          "Div": "AL",
-          "Pos": "Hitter",
-          "Comment": "He's an incredible all-around player."
-        },
-        {
-          "Name": "Mookie Betts",
-          "Comment": "Known for his exceptional fielding skills."
-        },
-        {
-          "Name": "Ronald Acuña Jr.",
-          "Comment": "A dynamic player with speed and power."
-        },
-        {
-          "Name": "Jacob deGrom",
-          "Comment": "Dominant pitcher with an electric fastball."
-        }
-      ]
-    },
-    {
-      "WeekNumber": "June",
-      "Players": [
-        {
-          "Name": "Fernando Tatis Jr.",
-          "Comment": "His energy and flair bring excitement to the game."
-        },
-        {
-          "Name": "Shohei Ohtani",
-          "Comment": "A two-way star, excelling as both a pitcher and hitter."
-        },
-        {
-          "Name": "Freddie Freeman",
-          "Comment": "Consistent hitter and leader for the Atlanta Braves."
-        },
-        {
-          "Name": "Gerrit Cole",
-          "Comment": "Crafty pitcher with excellent command and repertoire."
-        }
-      ]
-    }
-  ]
+
+
+  let data = {
+"Year": 2023,
+"Weeks": [
+  {
+    "WeekNumber": "1/2",
+    "Players": [
+      {
+        "Name": "Mike Trout",
+        "Div": "AL",
+        "Pos": "Hitter",
+        "Comment": "He's an incredible all-around player."
+      },
+      {
+        "Name": "Mookie Betts",
+        "Comment": "Known for his exceptional fielding skills."
+      },
+      {
+        "Name": "Ronald Acuña Jr.",
+        "Comment": "A dynamic player with speed and power."
+      },
+      {
+        "Name": "Jacob deGrom",
+        "Comment": "Dominant pitcher with an electric fastball."
+      }
+    ]
+  },
+  {
+    "WeekNumber": "3",
+    "Players": [
+      {
+        "Name": "Vladimir Guerrero Jr.",
+        "Div": "AL",
+        "Pos": "Hitter",
+        "Comment": "Continues to live up to his family's legacy."
+      },
+      {
+        "Name": "Fernando Valenzuela",
+        "Comment": "Mexican lefty known for his screwball pitch."
+      },
+      {
+        "Name": "Bryce Harper",
+        "Comment": "Powerful hitter and charismatic presence."
+      },
+      {
+        "Name": "Gleyber Torres",
+        "Comment": "Young talent with a promising future."
+      }
+    ]
+  },
+  {
+    "WeekNumber": "4",
+    "Players": [
+      {
+        "Name": "Shohei Ohtani",
+        "Div": "AL",
+        "Pos": "Two-Way Star",
+        "Comment": "An extraordinary talent on the mound and at the plate."
+      },
+      {
+        "Name": "Max Scherzer",
+        "Comment": "Crafty veteran pitcher with multiple Cy Young awards."
+      },
+      {
+        "Name": "Francisco Lindor",
+        "Comment": "Dynamic shortstop with a mix of power and defense."
+      },
+      {
+        "Name": "Anthony Rendon",
+        "Comment": "Solid third baseman with great hitting skills."
+      }
+    ]
+  },
+  {
+    "WeekNumber": "5",
+    "Players": [
+      {
+        "Name": "Freddie Freeman",
+        "Div": "NL",
+        "Pos": "Hitter",
+        "Comment": "Consistent hitter and leader for the Atlanta Braves."
+      },
+      {
+        "Name": "Clayton Kershaw",
+        "Comment": "Crafty left-handed pitcher and Dodgers icon."
+      },
+      {
+        "Name": "José Abreu",
+        "Comment": "Powerful first baseman with a great batting eye."
+      },
+      {
+        "Name": "Xander Bogaerts",
+        "Comment": "Smooth-fielding shortstop with offensive prowess."
+      }
+    ]
+  },
+  {
+    "WeekNumber": "May",
+    "Players": [
+      {
+        "Name": "Aaron Judge",
+        "Comment": "Massive power hitter and fan favorite."
+      },
+      {
+        "Name": "Walker Buehler",
+        "Comment": "Young pitcher with a fiery fastball and composure."
+      },
+      {
+        "Name": "Yordan Álvarez",
+        "Comment": "Slugger with incredible raw power."
+      },
+      {
+        "Name": "Trevor Story",
+        "Comment": "Shortstop known for his defensive skills and homers."
+      }
+    ]
+  }
+]
+}
+let numWeeksToShow = 1;
+function addWeek() {
+  // Increase the number of weeks to show if it's less than the total number of weeks
+  if (numWeeksToShow < data.Weeks.length) {
+    numWeeksToShow += 1;
+  }
 }
 
-// onMount(async () => {
-//     const res = await fetch(`/mlb/endpoint`);
-//     data = await res.json();
-//   });
-
-</script>
-
-
-<div class=" border-black rounded-sm border-2 w-7/12 mt-4">
-  <table class="w-full h-full">
-    <thead>
-      <tr class="w-full text-left bg-teal-400 text-white border-black border-b-2">
-        <th class="p-2">Week</th>
-        <th class="p-2">Name</th>
-        <th class="p-2">Comment</th>
-      </tr>
-    </thead>
-    <tbody>
-      {#each data.Weeks as week}
+function removeWeek() {
+  // Decrease the number of weeks to show if it's greater than 1
+  if (numWeeksToShow >= 1) {
+    numWeeksToShow -= 1;
+  }
+}
+  </script>
+  
+<div class="border-black rounded-sm border-2 w-7/12 mt-4">
+<table class="w-full h-full">
+  <thead>
+    <tr class="w-full text-left bg-teal-400 text-white border-black border-b-2">
+      <th class="p-2">Week</th>
+      <th class="p-2">Name</th>
+      <th class="p-2">Comment</th>
+    </tr>
+  </thead>
+  <tbody>
+    {#each data.Weeks.slice(0, numWeeksToShow) as week}
       <tr class="border-r-0 border-l-0">
         <td colspan="3" class="p-2 font-semibold text-black">
           { !isNaN(week.WeekNumber[0]) ? "Week " + week.WeekNumber : week.WeekNumber}
         </td>
       </tr>
       {#each week.Players as player, index}
-      <tr class="{index % 2 === 0 ? 'bg-gray-100' : 'bg-gray-200'}">
-        <td class="p-2"></td>
-        <td class="p-2">{player.Name}</td>
-        <td class="p-2">{player.Comment}</td>
-      </tr>
+        <tr class="{index % 2 === 0 ? 'bg-gray-100' : 'bg-gray-200'}">
+          <td class="p-2"></td>
+          <td class="p-2">{player.Name}</td>
+          <td class="p-2">{player.Comment}</td>
+        </tr>
       {/each}
-      {/each}
-    </tbody>
-  </table>
+    {/each}
+  </tbody>
+</table>
 </div>
+
+<div class="flex space-x-3">
+<button on:click={addWeek} class="mt-4 bg-blue-500 text-white font-semibold px-4 py-2 rounded hover:bg-blue-600 w-22">
+Add week
+</button>
+<button on:click={removeWeek} class="mt-4 bg-blue-500 text-white font-semibold px-4 py-2 rounded hover:bg-blue-600 w-22">
+  Remove Week
+</button>
+</div> 
--- my-app/src/routes/Results.svelte
+++ my-app/src/routes/Results.svelte
@@ -1,215 +1,274 @@
 <script> 
 
-import {voted, currentDisplay} from "./stores.js";
-function ballots() {
-    voted.set(!$voted);
-    currentDisplay.set(0);
-  }
-
-
-let data = {
-  "Results": {
-    "AL": {
-      "Pitchers": [
-        {
-          "Name": "Pitcher Name 1",
-          "Votes": 7,
-          "Comments": [{"Username": "Commenter 1", "Comment": "Comment 1"}, {"Username": "Commenter 2", "Comment": "Comment 2"}, {"Username": "Commenter 3", "Comment": "Comment 3"}]
-        },
-        {
-          "Name": "Pitcher Name 2",
-          "Votes": 3,
-          "Comments": [{"Username": "Commenter 4", "Comment": "Comment 4"}]
-        }
-      ],
-      "Hitters": [
-        {
-          "Name": "Hitter Name 1",
-          "Votes": 1,
-          "Comments": [{"Username": "Commenter 5", "Comment": "Comment 5"}]
-        },
-        {
-          "Name": "Hitter Name 2",
-          "Votes": 2,
-          "Comments": [{"Username": "Commenter 6", "Comment": "Comment 6"}, {"Username": "Commenter 7", "Comment": "Comment 7"}]
-        }
-      ]
-    },
-    "NL": {
-      "Pitchers": [
-        {
-          "Name": "Pitcher Name 1",
-          "Votes": 6,
-          "Comments": [{"Username": "Commenter 8", "Comment": "Comment 8"}]
-        },
-        {
-          "Name": "Pitcher Name 2",
-          "Votes": 4,
-          "Comments": [{"Username": "Commenter 9", "Comment": "Comment 9"}, {"Username": "Commenter 10", "Comment": "Comment 10"}, {"Username": "Commenter 11", "Comment": "Comment 11"}]
-        }
-      ],
-      "Hitters": [
-        {
-          "Name": "Hitter Name 1",
-          "Votes": 5,
-          "Comments": [{"Username": "Commenter 12", "Comment": "Comment 12"}, {"Username": "Commenter 13", "Comment": "Comment 13"}]
-        },
-        {
-          "Name": "Hitter Name 2",
-          "Votes": 8,
-          "Comments": [{"Username": "Commenter 14", "Comment": "Comment 14"}]
-        }
-      ]
+  import {voted, currentDisplay} from "./stores.js";
+  function ballots() {
+      voted.set(!$voted);
+      currentDisplay.set(0);
     }
+  
+  
+  let data = {
+"Results": {
+  "AL": {
+    "Pitchers": [
+      {
+        "Name": "Justin Verlander",
+        "Votes": 92,
+        "Comments": [
+          {"Username": "BaseballFan123", "Comment": "Verlander is a true ace."},
+          {"Username": "PitchingAce", "Comment": "He's been carrying the team on his back."},
+          {"Username": "MLBExpert99", "Comment": "Verlander's experience is evident in his performance."}
+        ]
+      },
+      {
+        "Name": "Gerrit Cole",
+        "Votes": 87,
+        "Comments": [
+          {"Username": "AstrosNation", "Comment": "Cole's strikeouts are a sight to see."},
+          {"Username": "FastballFanatic", "Comment": "Cole's fastball is electric."},
+          {"Username": "CurveballKing", "Comment": "His curveball is a work of art."}
+        ]
+      }
+    ],
+    "Hitters": [
+      {
+        "Name": "Mike Trout",
+        "Votes": 95,
+        "Comments": [
+          {"Username": "AngelsFan22", "Comment": "Trout is the best player in the game."},
+          {"Username": "HomeRunHero", "Comment": "Another day, another Trout home run."},
+          {"Username": "MVPChants", "Comment": "Trout's consistency is astonishing."}
+        ]
+      },
+      {
+        "Name": "José Altuve",
+        "Votes": 78,
+        "Comments": [
+          {"Username": "AstrosLover", "Comment": "Altuve's small stature doesn't stop him."},
+          {"Username": "BatFlipMaster", "Comment": "Altuve's bat flips are legendary."}
+        ]
+      }
+    ]
+  },
+  "NL": {
+    "Pitchers": [
+      {
+        "Name": "Jacob deGrom",
+        "Votes": 93,
+        "Comments": [
+          {"Username": "MetsMania", "Comment": "deGrom is the heart and soul of the Mets."},
+          {"Username": "StrikeoutMachine", "Comment": "His strikeouts are a work of art."},
+          {"Username": "AmazingSlider", "Comment": "deGrom's slider is unhittable."}
+        ]
+      },
+      {
+        "Name": "Max Scherzer",
+        "Votes": 88,
+        "Comments": [
+          {"Username": "NatsNation", "Comment": "Scherzer's intensity is unmatched."},
+          {"Username": "MadMaxFanatic", "Comment": "Max is a true warrior on the mound."},
+          {"Username": "ScherzerSizzle", "Comment": "Scherzer's passion for the game is inspiring."}
+        ]
+      }
+    ],
+    "Hitters": [
+      {
+        "Name": "Freddie Freeman",
+        "Votes": 85,
+        "Comments": [
+          {"Username": "BravesCountry", "Comment": "Freddie Freeman is a Braves legend."},
+          {"Username": "ClutchHitsOnly", "Comment": "Freddie delivers when it matters most."},
+          {"Username": "NL_MVP", "Comment": "He's a strong contender for the NL MVP."}
+        ]
+      },
+      {
+        "Name": "Mookie Betts",
+        "Votes": 89,
+        "Comments": [
+          {"Username": "DodgersDynasty", "Comment": "Mookie Betts is a game-changer."},
+          {"Username": "BettsBeliever", "Comment": "Betts' all-around skills are unmatched."},
+          {"Username": "SpeedDemon", "Comment": "Mookie's speed adds another dimension to his game."}
+        ]
+      }
+    ]
   }
 }
+}
 
-let month=  {
-  "Results": {
-    "AL": {
-      "Pitchers": [
-        {
-          "Name": "test name",
-          "Votes": 7,
-          "Comments": [{"Username": "Commenter 1", "Comment": "Comment 1"}, {"Username": "Commenter 2", "Comment": "Comment 2"}, {"Username": "Commenter 3", "Comment": "Comment 3"}]
-        },
-        {
-          "Name": "Pitcher Name 2",
-          "Votes": 3,
-          "Comments": [{"Username": "Commenter 4", "Comment": "Comment 4"}]
-        }
-      ],
-      "Hitters": [
-        {
-          "Name": "Hitter Name 1",
-          "Votes": 1,
-          "Comments": [{"Username": "Commenter 5", "Comment": "Comment 5"}]
-        },
-        {
-          "Name": "Hitter Name 2",
-          "Votes": 2,
-          "Comments": [{"Username": "Commenter 6", "Comment": "Comment 6"}, {"Username": "Commenter 7", "Comment": "Comment 7"}]
-        }
-      ]
-    },
-    "NL": {
-      "Pitchers": [
-        {
-          "Name": "Pitcher Name 1",
-          "Votes": 6,
-          "Comments": [{"Username": "Commenter 8", "Comment": "Comment 8"}]
-        },
-        {
-          "Name": "Pitcher Name 2",
-          "Votes": 4,
-          "Comments": [{"Username": "Commenter 9", "Comment": "Comment 9"}, {"Username": "Commenter 10", "Comment": "Comment 10"}, {"Username": "Commenter 11", "Comment": "Comment 11"}]
-        }
-      ],
-      "Hitters": [
-        {
-          "Name": "Hitter Name 1",
-          "Votes": 5,
-          "Comments": [{"Username": "Commenter 12", "Comment": "Comment 12"}, {"Username": "Commenter 13", "Comment": "Comment 13"}]
-        },
-        {
-          "Name": "Hitter Name 2",
-          "Votes": 8,
-          "Comments": [{"Username": "Commenter 14", "Comment": "Comment 14"}]
-        }
-      ]
-    }
+  
+  let month=  {
+"Results": {
+  "AL": {
+    "Pitchers": [
+      {
+        "Name": "James Paxton",
+        "Votes": 12,
+        "Comments": [
+          {"Username": "PitchingFan1", "Comment": "Paxton's curveball is a thing of beauty."}
+        ]
+      },
+      {
+        "Name": "Zack Greinke",
+        "Votes": 8,
+        "Comments": [
+          {"Username": "AstrosNation", "Comment": "Greinke's changeup is masterful."},
+          {"Username": "GreinkeFanatic", "Comment": "I'm always amazed by his control on the mound."}
+        ]
+      }
+    ],
+    "Hitters": [
+      {
+        "Name": "Anthony Rendon",
+        "Votes": 7,
+        "Comments": [
+          {"Username": "AngelsFan22", "Comment": "Rendon's swing is a work of art."}
+        ]
+      },
+      {
+        "Name": "Gleyber Torres",
+        "Votes": 4,
+        "Comments": [
+          {"Username": "YankeesNation", "Comment": "Torres has a bright future ahead."},
+          {"Username": "DoublePlayMaster", "Comment": "His defensive plays are impressive."}
+        ]
+      }
+    ]
+  },
+  "NL": {
+    "Pitchers": [
+      {
+        "Name": "Walker Buehler",
+        "Votes": 10,
+        "Comments": [
+          {"Username": "DodgersDynasty", "Comment": "Buehler's fastball has serious heat."},
+          {"Username": "PitchingAce", "Comment": "His composure on the mound is remarkable."}
+        ]
+      },
+      {
+        "Name": "Yu Darvish",
+        "Votes": 6,
+        "Comments": [
+          {"Username": "CubsNation", "Comment": "Darvish's pitch repertoire keeps hitters off balance."}
+        ]
+      }
+    ],
+    "Hitters": [
+      {
+        "Name": "Ronald Acuña Jr.",
+        "Votes": 9,
+        "Comments": [
+          {"Username": "BravesCountry", "Comment": "Acuña Jr.'s speed adds excitement to the game."},
+          {"Username": "PowerHitter", "Comment": "His home runs are a thing of beauty."}
+        ]
+      },
+      {
+        "Name": "Fernando Tatis Jr.",
+        "Votes": 11,
+        "Comments": [
+          {"Username": "PadresFanatic", "Comment": "Tatis Jr. is a spark plug for the Padres."},
+          {"Username": "DynamicPlayer", "Comment": "His energy on the field is infectious."},
+          {"Username": "SwagSwing", "Comment": "Tatis Jr.'s unique swing is captivating."}
+        ]
+      }
+    ]
   }
+}
 }
 
 
-
-</script>
-
-<div class="w-8/12 mx-auto mb-12">
-  {#each Object.entries(data.Results) as [league, category]}
-    <h2 class="text-2xl font-bold mt-4 mb-2 underline">{league}</h2>
-    {#each Object.entries(category) as [categoryName, players]}
-      <h3 class="text-xl font-semibold mt-2 mb-1">{categoryName}</h3>
-      <table class="w-full border-collapse border overflow-hidden bg-black  rounded-lg ">
-        <thead>
-          <tr class="bg-teal-400 text-white">
-            <th class="px-4 py-2 font-medium text-left">Player</th>
-            <th class="px-4 py-2 font-medium text-left">Votes</th>
-          </tr>
-        </thead>
-        <tbody>
-          {#each players as player, index}
-          <tr class="{index % 2 === 0 ? 'bg-gray-200' : 'bg-gray-300'}">
-            <td class="px-4 py-2 border w-8/12">{player.Name}</td>
-            <td class="px-4 py-2 border w-4/12">
-              <button class="bg-teal-400 hover:bg-teal-500 active:bg-teal-600 text-white px-2 py-1 rounded w-full {player.showDetails ? 'bg-teal-600' : ''}" on:click={() => (player.showDetails = !player.showDetails)}>
-                {player.Votes}
-              </button>
-            </td>
-          </tr>
-          {#if player.showDetails}
+  
+  let isMonthly = month.length;
+  
+  </script>
+  
+  <div class="w-8/12 mx-auto mb-12">
+    {#each Object.entries(data.Results) as [league, category]}
+      <h2 class="text-2xl font-bold mt-4 mb-2 underline">{league}</h2>
+      {#each Object.entries(category) as [categoryName, players]}
+        <h3 class="text-xl font-semibold mt-2 mb-1">{categoryName}</h3>
+        <table class="w-full border-collapse border overflow-hidden bg-black  rounded-lg ">
+          <thead>
+            <tr class="bg-teal-400 text-white">
+              <th class="px-4 py-2 font-medium text-left">Player</th>
+              <th class="px-4 py-2 font-medium text-left">Votes</th>
+            </tr>
+          </thead>
+          <tbody>
+            {#each players as player, index}
             <tr class="{index % 2 === 0 ? 'bg-gray-200' : 'bg-gray-300'}">
-              <td class="px-4 py-2 border" colspan="2">
-                <ul class="list-disc list-inside">
-                  {#each player.Comments as comment}
-                    <li>
-                      <strong>{comment.Username}</strong>: {comment.Comment}
-                    </li>
-                  {/each}
-                </ul>
+              <td class="px-4 py-2 border w-8/12">{player.Name}</td>
+              <td class="px-4 py-2 border w-4/12">
+                <button class="bg-teal-400 hover:bg-teal-500 active:bg-teal-600 text-white px-2 py-1 rounded w-full {player.showDetails ? 'bg-teal-600' : ''}" on:click={() => (player.showDetails = !player.showDetails)}>
+                  {player.Votes}
+                </button>
               </td>
             </tr>
-          {/if}
-          {/each}
-        </tbody>
-      </table>
+            {#if player.showDetails}
+              <tr class="{index % 2 === 0 ? 'bg-gray-200' : 'bg-gray-300'}">
+                <td class="px-4 py-2 border" colspan="2">
+                  <ul class="list-disc list-inside">
+                    {#each player.Comments as comment}
+                      <li>
+                        <strong>{comment.Username}</strong>: {comment.Comment}
+                      </li>
+                    {/each}
+                  </ul>
+                </td>
+              </tr>
+            {/if}
+            {/each}
+          </tbody>
+        </table>
+      {/each}
     {/each}
-  {/each}
-</div>
-
-{#if (month)}
-<p class="text-5xl"> Monthly Results </p>
-
-<div class="w-8/12 mx-auto mb-12">
-  {#each Object.entries(month.Results) as [league, category]}
-    <h2 class="text-2xl font-bold mt-4 mb-2 underline">{league}</h2>
-    {#each Object.entries(category) as [categoryName, players]}
-      <h3 class="text-xl font-semibold mt-2 mb-1">{categoryName}</h3>
-      <table class="w-full border-collapse border overflow-hidden bg-black  rounded-lg ">
-        <thead>
-          <tr class="bg-teal-400 text-white">
-            <th class="px-4 py-2 font-medium text-left">Player</th>
-            <th class="px-4 py-2 font-medium text-left">Votes</th>
-          </tr>
-        </thead>
-        <tbody>
-          {#each players as player, index}
-          <tr class="{index % 2 === 0 ? 'bg-gray-200' : 'bg-gray-300'}">
-            <td class="px-4 py-2 border w-8/12">{player.Name}</td>
-            <td class="px-4 py-2 border w-4/12">
-              <button class="bg-teal-400 hover:bg-teal-500 active:bg-teal-600 text-white px-2 py-1 rounded w-full {player.showDetails ? 'bg-teal-600' : ''}" on:click={() => (player.showDetails = !player.showDetails)}>
-                {player.Votes}
-              </button>
-            </td>
-          </tr>
-          {#if player.showDetails}
+  </div>
+  
+  {#if isMonthly}
+  <p class="text-5xl"> Monthly Results </p>
+  
+  <div class="w-8/12 mx-auto mb-12">
+    {#each Object.entries(month.Results) as [league, category]}
+      <h2 class="text-2xl font-bold mt-4 mb-2 underline">{league}</h2>
+      {#each Object.entries(category) as [categoryName, players]}
+        <h3 class="text-xl font-semibold mt-2 mb-1">{categoryName}</h3>
+        <table class="w-full border-collapse border overflow-hidden bg-black  rounded-lg ">
+          <thead>
+            <tr class="bg-teal-400 text-white">
+              <th class="px-4 py-2 font-medium text-left">Player</th>
+              <th class="px-4 py-2 font-medium text-left">Votes</th>
+            </tr>
+          </thead>
+          <tbody>
+            {#each players as player, index}
             <tr class="{index % 2 === 0 ? 'bg-gray-200' : 'bg-gray-300'}">
-              <td class="px-4 py-2 border" colspan="2">
-                <ul class="list-disc list-inside">
-                  {#each player.Comments as comment}
-                    <li>
-                      <strong>{comment.Username}</strong>: {comment.Comment}
-                    </li>
-                  {/each}
-                </ul>
+              <td class="px-4 py-2 border w-8/12">{player.Name}</td>
+              <td class="px-4 py-2 border w-4/12">
+                <button class="bg-teal-400 hover:bg-teal-500 active:bg-teal-600 text-white px-2 py-1 rounded w-full {player.showDetails ? 'bg-teal-600' : ''}" on:click={() => (player.showDetails = !player.showDetails)}>
+                  {player.Votes}
+                </button>
               </td>
             </tr>
-          {/if}
-          {/each}
-        </tbody>
-      </table>
+            {#if player.showDetails}
+              <tr class="{index % 2 === 0 ? 'bg-gray-200' : 'bg-gray-300'}">
+                <td class="px-4 py-2 border" colspan="2">
+                  <ul class="list-disc list-inside">
+                    {#each player.Comments as comment}
+                      <li>
+                        <strong>{comment.Username}</strong>: {comment.Comment}
+                      </li>
+                    {/each}
+                  </ul>
+                </td>
+              </tr>
+            {/if}
+            {/each}
+          </tbody>
+        </table>
+      {/each}
     {/each}
-  {/each}
-</div>
+  </div>
+  
+  {/if}
 
-{/if}
+  <p> Toggle Monthly Status </p>
+  <input type="checkbox" class="form-checkbox h-5 w-5 text-indigo-600 mb-12" bind:checked={isMonthly}>
