bc2b4b1066bba4f85303773d811bf170757931f7
Alex@gdplace.com <Alex@gdplace.com>
2023-08-04 15:35:26 -0400

    Changed playerdata to list to help sort and it's faster too then processing so overall a really good change

--- my-app/src/routes/PlayerData.py
+++ my-app/src/routes/PlayerData.py
@@ -40,7 +40,7 @@ def get_team_abbreviation(team_id):
     return team_ids.get(team_id)
 
 
-statcast_data = statcast(start_dt="2023-01-01", end_dt="2023-08-04")
+statcast_data = statcast(start_dt="2023-08-03", end_dt="2023-08-04")
 
 
 player_names = statcast_data['player_name'].unique()
@@ -79,14 +79,19 @@ for name in sorted_formatted_player_names:
     except Exception as e:
         print(f"An error occurred for player {name}: {e}")
 
+player_strings = []
+for player_info in results:
+    full_name = player_info['Full name']
+    team = player_info['Team']
+    position = player_info['Position']
+    player_string = f"{full_name.split()[1]} {full_name.split()[0]} - {team} ({position})"
+    player_strings.append(player_string)
 
-json_results = json.dumps(results, indent=2)
+# Write the list to a JavaScript file
+with open('my-app\\src\\routes\\playerList.js', 'w') as file:
+    file.write('export let playersList = [\n')
+    for player_string in player_strings:
+        file.write(f'    "{player_string}",\n')
+    file.write('];')
 
-
-# Write the encoded player names to a JavaScript file with proper formatting
-js_code = f"export const playersList = {json_results};\n"
-js_filename = 'my-app/src/routes/playerList.js'
-with open(js_filename, 'w', encoding='utf-8') as js_file:
-    js_file.write(js_code)
-
-print("done!")
+print("Task Complete")
